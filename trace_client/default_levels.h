d686259942d17bdb4ee6573d2cce059234118257
Tomas Barak <bary@vylizprdel.cz>
2014-06-09 17:11:33 +0200

    fix missing getLevelDictionary (broken build)

--- trace_client/default_levels.h
+++ trace_client/default_levels.h
@@ -18,5 +18,21 @@ FACT_DECLARE_ENUM_TO_STRING(E_TraceLevel,TRACELEVEL_ENUM);
 
 typedef E_TraceLevel level_t;
 
+#define TRACE_INIT_LVL_DICT_PAIR(name,assign) {#name, e_##name},
+
+struct LvlDictPair {
+	char const * first;
+	level_t second;
+};
+
+inline size_t getLevelDictionnary (LvlDictPair const * & out)
+{
+	static const LvlDictPair s_dict[] = {
+		TRACELEVEL_ENUM(TRACE_INIT_LVL_DICT_PAIR)
+	};
+	out = s_dict;
+	return sizeof(s_dict) / sizeof(*s_dict);
+}
+
 }
 
