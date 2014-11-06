e460e9da87e93e9aa4e3f505d7311100e6b24c9f
Tomas Barak <bary@vylizprdel.cz>
2014-11-06 14:02:40 +0100

    * updated build doc

--- build/configure_static_qt5_whs.bat
+++ build/configure_static_qt5_whs.bat
@@ -1,4 +1,9 @@
-c:\"Program Files (x86)\Microsoft DirectX SDK (June 2010)\Utilities\bin\dx_setenv.cmd"
+"e:\devel\Microsoft DirectX SDK (June 2010)\Utilities\bin\dx_setenv.cmd"
+set PATH=e:\devel\Git\bin;%PATH%
+set PATH=e:\devel\strawberry-perl\perl\bin;%PATH%
+set PATH=e:\devel\qt\5.3.2\qtbase\bin;%PATH%
+set PATH=e:\devel\qt\5.3.2\gnuwin32\bin;%PATH%
+set PATH=e:\devel\Python34;%PATH%
 SET CL=/MP
-configure.bat -debug-and-release -opensource -platform win32-msvc2010 -static -ltcg -no-accessibility -no-openvg -no-gif -no-dbus -no-audio-backend -no-sql-sqlite -no-qml-debug -nomake examples -nomake tests -no-icu -angle
-rem configure.bat -debug-and-release -opensource -platform win32-msvc2010 -static -no-ltcg -no-accessibility -no-gif -no-dbus -no-audio-backend -no-sql-sqlite -nomake examples -nomake tests -no-nis -angle -no-directwrite -no-qml-debug -no-icu -MP
+configure.bat -debug-and-release -opensource -platform win32-msvc2013 -static -ltcg -no-accessibility -no-openvg -no-gif -no-dbus -no-audio-backend -no-sql-sqlite -no-qml-debug -nomake examples -nomake tests -no-icu -angle
+rem configure.bat -debug-and-release -opensource -platform win32-msvc2013 -static -no-ltcg -no-accessibility -no-gif -no-dbus -no-audio-backend -no-sql-sqlite -nomake examples -nomake tests -no-nis -angle -no-directwrite -no-qml-debug -no-icu -MP
